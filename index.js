f91f27d0ee164fe02510e6d246d4edd0b2a050bc
Arjun Kamath <junnaonly@gmail.com>
2015-04-27 11:04:22 +0300

    some changes..

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-io.configure(function () {  
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
-
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -15,32 +10,27 @@ app.get('/messages', function(req, res){
   res.sendFile(__dirname + '/messages.html');
 });
 
-
+/* io.configure(function () {  
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+ */
 var msgs = io.of('/messages');
 
 msgs.on('connection', function(socket){
   console.log('someone connected');
 });
-msgs.emit('hi', 'everyone!');
 
-io.on('connection', function(socket){ 
-  //var tweet = {user: "nodesource", text: "Hello, world!"};	
-	
-  /* socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  }); */
-  
+io.on('connection', function(socket){   
+  //io.set("transports", ["xhr-polling"]);
+  //io.set("polling duration", 10);
+
   socket.on("feedback", function(feedback){
     io.emit("feedback", feedback);
 	msgs.emit("feedback", feedback);
 	console.log(feedback);
   });
-  
 });
-
-
-
-
 
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening');
